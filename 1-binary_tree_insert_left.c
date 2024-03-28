8ae80d69c5d5865307493c63d5f06e1e055cb71f
elyessassi <mjoxen@gmail.com>
2024-03-28 12:56:53 +0000

    inserting new left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,8 +11,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+
 	binary_tree_t *newnode;
 	
+	if (parent == NULL)
+		return (NULL);
+		
 	newnode = binary_tree_node(parent, value);
 
 	if (parent->left == NULL)
