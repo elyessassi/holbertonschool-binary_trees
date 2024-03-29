0f7f0f0a5f1135c7b72dd11035a62dc8970ba257
elyessassi <mjoxen@gmail.com>
2024-03-29 13:11:25 +0000

    counting leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,11 +8,11 @@
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
-{	
+{
 	if (tree == NULL)
 		return (0);
-	
-	if (tree->left == NULL && tree->right == NULL )
+
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (1);
 	}
