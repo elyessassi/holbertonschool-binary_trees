61e330bf1f4032a1aa4aa8b7d7dba92136179143
elyessassi <mjoxen@gmail.com>
2024-03-29 01:13:59 +0000

    get height of a tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,33 +1,29 @@
 #include "binary_trees.h"
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdbool.h>
+
 
 /**
- * binary_tree_postorder - go throught a binary tree in a postorder order
- * @tree: pointer to a tree
- * @func: function to apply on data
+ * binary_tree_height - a function that measures the height of a binary tree
+ * @tree: pointer to the first node of tree
+ * Return: height of binary tree
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_h;
-	size_t right_h;
+	size_t left_h = 0;
+	size_t right_h = 0;
 
-
-	if (!tree)
-	{
+	if (tree == NULL)
 		return (0);
-	}
+
+	right_h = binary_tree_height(tree->left);
+	left_h = binary_tree_height(tree->right);
+
+
+	if (tree->right == NULL && tree->left == NULL)
+		return (0);
+
+	if (right_h >= left_h)
+		return (right_h + 1);
 	else
-	{
-		left_h = binary_tree_height(tree->left) + 1;
-		right_h = binary_tree_height(tree->right) + 1;
-	}
-	if (left_h > right_h)
-		return (left_h) + 1;
-	else
-		return (right_h) + 1;
-	
+		return (left_h + 1);
 }
