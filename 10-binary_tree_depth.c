55c64a73d8a71955ab029fb10460f50d0d07ac37
elyessassi <mjoxen@gmail.com>
2024-03-29 10:54:42 +0000

    calculate the depth of a node

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_depth - a function that measures the max depth of a binary tree
+ * @tree: pointer to the first node of tree
+ * Return: depth of binary tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    int depth = 0;
+
+	if (tree == NULL)
+		return (0);
+
+    while (tree->parent)
+    {
+        depth += 1;
+        tree = tree->parent;
+    }
+    return (depth);
+}
