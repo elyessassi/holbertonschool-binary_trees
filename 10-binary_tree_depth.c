6325b80ee189ac7134a71a653a5d49bf99dbc04e
elyessassi <mjoxen@gmail.com>
2024-03-29 10:56:04 +0000

    calculate the depth of a node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,15 +9,15 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    int depth = 0;
+	int depth = 0;
 
 	if (tree == NULL)
 		return (0);
 
-    while (tree->parent)
-    {
-        depth += 1;
-        tree = tree->parent;
-    }
-    return (depth);
+	while (tree->parent)
+	{
+		depth += 1;
+		tree = tree->parent;
+	}
+	return (depth);
 }
