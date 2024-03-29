882d753d79553334ca5e739ac45f926a11b8a53c
elyessassi <mjoxen@gmail.com>
2024-03-29 11:14:01 +0000

    calculate size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,5 +13,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return(binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
