8ec41be3455750ccec226a33156137df1e26f639
elyessassi <mjoxen@gmail.com>
2024-04-01 09:28:21 +0000

    check if a bt is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -41,16 +41,16 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->lefttft == NULL && tree->righttght == NULL))
+	if ((tree->left == NULL && tree->right == NULL))
 		return (1);
-	if ((tree->lefttft && !tree->righttght) || (!tree->lefttft && tree->righttght))
+	if ((tree->left && !tree->right) || (!tree->left && tree->right))
 		return (0);
 
-	leftt = binary_tree_height(tree->lefttft);
-	rightt = binary_tree_height(tree->righttght);
+	leftt = binary_tree_height(tree->left);
+	rightt = binary_tree_height(tree->right);
 
-	if (binary_tree_is_perfect(tree->lefttft) &&
-		binary_tree_is_perfect(tree->righttght) && 
+	if (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right) && 
         (rightt == leftt))
 	{
 		return (1);
